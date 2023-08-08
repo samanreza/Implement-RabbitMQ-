485bb02f798a628c7958c69536dfb9c995758f9b
Saman Farabi <saman.farabi1818@gmail.com>
2023-08-08 16:30:12 +0330

    queue added to class

--- app/Notifications/SendSmsClass.php
+++ app/Notifications/SendSmsClass.php
@@ -10,7 +10,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class SendSmsClass extends Notification
+class SendSmsClass extends Notification implements ShouldQueue
 {
     use Queueable;
 
